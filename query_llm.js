33c9cfe86f767ddc4f35033b953ea4c365c4ded6
Hugues D'hardemare <hugues.du-moulinet-dhardemare@student-cs.fr>
2024-04-27 10:15:22 +0200

    function prompt

--- query_llm.js
+++ query_llm.js
@@ -1,14 +1,47 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { PromptTemplate } from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
+import { LLMChain } from "langchain/chains";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const model = new ChatOpenAI({
-    openAIApiKey: process.env.GPT_API_KEY,
-    modelName: "gpt-3.5-turbo",
-    temperature: 0.9,
-    maxTokens: 100,
-    });
+const AWS_S3 = new S3Client({ region: "eu-north-1" }); // Adjust the region if necessary
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-const response = await model.invoke("Write a poem about AI");
-console.log("AI :", response.content);
+export const query = async () => {
+
+    const memory = new BufferMemory({ memoryKey: "chat_history" });
+
+    const model = new ChatOpenAI({
+        openAIApiKey: OPENAI_API_KEY,
+        modelName: "gpt-3.5-turbo",
+        temperature: 0.8,
+        maxTokens: 100,
+        });
+
+    const embeddings = new OpenAIEmbeddings({
+        openAIApiKey: OPENAI_API_KEY,
+        });
+
+    const template = `It is a conversation between the human user and an AI. The AI should answer to the user's questions taking in context the chat history.
+    Current conversation:
+    {chat_history}
+    Human: {input}
+    AI:`;
+
+    const prompt = PromptTemplate.fromTemplate(template);
+
+    const chain = new LLMChain({ llm: model, prompt, memory });
+
+    const response = await chain.call({ input: "Hello! I'm Hugues. How are you?" });
+    console.log({ response });
+
+    // const response2 = await chain.call({ input: "Nothing. Do you remember my name?" });
+    // console.log({ response2 });
+  
+    };
+
+query();
